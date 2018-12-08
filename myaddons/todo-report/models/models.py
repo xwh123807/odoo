cb82144efb3a932f03805aaf809275c5691fa5cb
xiangwanhong <xiangwanhong@192.168.49.200>
2018-12-08 15:05:28 +0800

    增加发送报告功能

--- myaddons/todo-report/models/models.py
+++ myaddons/todo-report/models/models.py
@@ -2,14 +2,41 @@
 
 from odoo import models, fields, api
 
-# class todo-report(models.Model):
-#     _name = 'todo-report.todo-report'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class TodoTask(models.Model):
+     _inherit = 'todo.task'
+     
+     @api.multi
+     def action_report_send(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = ir_model_data.get_object_reference('todo-report', 'email_template_todo_task')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict(self.env.context or {})
+        ctx.update({
+            'default_model': 'todo.task',
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'custom_layout': "mail.mail_notification_paynow",
+            'force_email': True,
+            'mark_rfq_as_sent': True,
+        })
+        return {
+            'name': 'Compose Email',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
