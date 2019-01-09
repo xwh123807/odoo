ba5a753cdbb0c803d5d952fc8d654870553d371c
xwh123807 <xwh123807@gmail.com>
2019-01-09 23:33:05 +0800

    增加报告

--- myaddons/hr-evaluation/__manifest__.py
+++ myaddons/hr-evaluation/__manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        'reports/hr-post-report.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
--- myaddons/hr-evaluation/models/models.py
+++ myaddons/hr-evaluation/models/models.py
@@ -23,6 +23,9 @@ class Post(models.Model):
     postlevel_ids = fields.One2many('hr.postlevel', "post_id", string='Post Level')
     postskill_ids = fields.One2many('hr.postskill', 'post_id', string='Post Skill')
     skillscore_ids = fields.One2many('hr.skillscore', 'post_id', string='Skill Score')
+    postscore_ids = fields.One2many('hr.postscore', 'post_id', string='Post Score')
+    postcondition_ids = fields.One2many('hr.postcondition', 'post_id', string='Post Condition')
+    postdesc_ids = fields.One2many('hr.postdesc', 'post_id', string='Post Qualification')
 
 
 class PostLevel(models.Model):
@@ -37,6 +40,13 @@ class PostLevel(models.Model):
     description = fields.Text('Description')
     postcondition_ids = fields.One2many('hr.postcondition', 'postlevel_id', string="Post Condition")
     postdesc_ids = fields.One2many('hr.postdesc', 'postlevel_id', string='Post Qualification')
+    postscore_ids = fields.One2many('hr.postscore', 'postlevel_id', string='Post Score')
+    total_score = fields.Integer('Total Score', compute='_compute_total_score', store=True)
+    
+    @api.depends('postscore_ids')
+    def _compute_total_score(self):
+        for line in self:
+            line.total_score = sum(line.postscore_ids.mapped('score'))
 
     
 class Skill(models.Model):
@@ -57,29 +67,7 @@ class PostSkill(models.Model):
     
     post_id = fields.Many2one('hr.post', string='Post', required=True, help='Post');
     skill_id = fields.Many2one('hr.skill', string='Skill', required=True, help='Skill')
-
-
-class PostCondition(models.Model):
-    """
-        岗级基本条件
-    """
-    _name = 'hr.postcondition'
-    
-    postlevel_id = fields.Many2one('hr.postlevel', string="Post Level", required=True)
-    requirement_condition = fields.Text(string='Requirement')
-    standard_condition = fields.Text(string='Standard')
-    level_id = fields.Char(string='Level', related='postlevel_id.name', readonly=True)
-
-
-class PostDesc(models.Model):
-    """
-        任职资格描述
-    """
-    _name = 'hr.postdesc'
-    
-    postlevel_id = fields.Many2one('hr.postlevel', string="Post Level", required=True)
-    skill_id = fields.Many2one('hr.skill', string='Skill', required=True, help='Skill')
-    qualification = fields.Text('Qualification')
+    skill_description = fields.Text('Skill Description', related='skill_id.description', readonly=True)
 
 
 class SkillScore(models.Model):
@@ -93,6 +81,33 @@ class SkillScore(models.Model):
     score = fields.Integer(string='Score')
     description = fields.Text(string='Description')
 
+
+class PostCondition(models.Model):
+    """
+        岗级基本条件
+    """
+    _name = 'hr.postcondition'
+    
+    postlevel_id = fields.Many2one('hr.postlevel', string="Post Level", required=True)
+    requirement_condition = fields.Text(string='Requirement')
+    standard_condition = fields.Text(string='Standard')
+    level_id = fields.Many2one('hr.level', string='Level', related='postlevel_id.level_id', readonly=True)
+    post_id = fields.Many2one('hr.post', string='Post', related='postlevel_id.post_id', readonly=True, store=True)
+
+
+class PostDesc(models.Model):
+    """
+        任职资格描述
+    """
+    _name = 'hr.postdesc'
+    
+    postlevel_id = fields.Many2one('hr.postlevel', string="Post Level", required=True)
+    skill_id = fields.Many2one('hr.skill', string='Skill', required=True, help='Skill')
+    qualification = fields.Text('Qualification')
+    level_id = fields.Many2one('hr.level', string='Level', related='postlevel_id.level_id', readonly=True)
+    post_id = fields.Many2one('hr.post', string='Post', related='postlevel_id.post_id', readonly=True, store=True)
+
+
 class PostScore(models.Model):
     """
         任职资格-得分
@@ -102,3 +117,4 @@ class PostScore(models.Model):
     postlevel_id = fields.Many2one('hr.postlevel', string="Post Level", required=True)
     skill_id = fields.Many2one('hr.skill', string='Skill', required=True, help='Skill')
     score = fields.Integer(string='Score')
+    post_id = fields.Many2one('hr.post', string='Post', related='postlevel_id.post_id', readonly=True, store=True)
