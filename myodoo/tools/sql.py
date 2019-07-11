fa3239eb72219d3f8b42d8471ffc5979ae23d6b2
xwh123807 <xwh123807@gmail.com>
2019-07-11 19:29:43 +0800

    add

--- myodoo/tools/sql.py
+++ myodoo/tools/sql.py
@@ -64,3 +64,192 @@ def create_model_table(cr, tablename, comment=None):
     if comment:
         cr.execute('COMMENT ON TABLE "%S" IS %s'.format(tablename, (comment,)))
     _schema.debug('Table %r: created', tablename)
+
+
+def table_columns(cr, tablename):
+    """
+        Return a dict mapping column name to their configuration. The latter is
+        a dict with the data from the table ''information_schema.columns''
+    :param cr:
+    :param tablename:
+    :return:
+    """
+    query = '''SELECT column_name, udt_name, character_maximum_length, is_nullable
+        FROM information_schema.columns WHERE table_name=%s'''
+    cr.execute(query, (tablename,))
+    return {row['column_name']: row for row in cr.dictfetchall()}
+
+
+def column_exists(cr, tablename, columnname):
+    """
+        Return whether the diven column exists.
+    :param cr:
+    :param tablename:
+    :param columnname:
+    :return:
+    """
+    query = """ SELECT 1 FROM information_schema.columns 
+        WHERE table_name=%s and column_name=%s """
+    cr.execute(query, (tablename, columnname))
+    return cr.rowcount
+
+
+def create_column(cr, tablename, columnname, columntype, comment=None):
+    """
+        Create a column with the given type.
+    :param cr:
+    :param tablename:
+    :param columnname:
+    :param columntype:
+    :param comment:
+    :return:
+    """
+
+
+def rename_column(cr, tablename, columnname1, columnname2):
+    """
+
+    :param cr:
+    :param tablename:
+    :param columnname1:
+    :param columnname2:
+    :return:
+    """
+
+
+def convert_column(cr, tablename, columnname, columntype):
+    """
+
+    :param cr:
+    :param tablename:
+    :param columnname:
+    :param columntype:
+    :return:
+    """
+
+
+def set_not_null(cr, tablename, columnname):
+    """
+
+    :param cr:
+    :param tablename:
+    :param columnname:
+    :return:
+    """
+
+
+def drop_not_null(cr, tablename, columnname):
+    """
+
+    :param cr:
+    :param tablename:
+    :param columnname:
+    :return:
+    """
+
+
+def constraint_definition(cr, tablename, constraintname):
+    """
+
+    :param cr:
+    :param tablename:
+    :param constraintname:
+    :return:
+    """
+
+
+def add_constraint(cr, tablename, constraintname, definition):
+    """
+
+    :param cr:
+    :param tablename:
+    :param constraintname:
+    :param definition:
+    :return:
+    """
+
+
+def drop_constraint(cr, tablename, constraintname):
+    """
+
+    :param cr:
+    :param tablename:
+    :param constraintname:
+    :return:
+    """
+
+
+def add_foreign_key(cr, tablename, columnname1, tablename2, columnname2, ondelete):
+    """
+
+    :param cr:
+    :param tablename:
+    :param columnname1:
+    :param tablename2:
+    :param columnname2:
+    :param ondelete:
+    :return:
+    """
+
+
+def fix_foreign_key(cr, tablename1, columnname1, tablename2, columnname2, ondelete):
+    """
+
+    :param cr:
+    :param tablename1:
+    :param columnname1:
+    :param tablename2:
+    :param columnname2:
+    :param ondelete:
+    :return:
+    """
+
+
+def index_exists(cr, indexname):
+    """
+
+    :param cr:
+    :param indexname:
+    :return:
+    """
+
+
+def create_index(cr, indexname, tablename, expressions):
+    """
+
+    :param cr:
+    :param indexname:
+    :param tablename:
+    :param expressions:
+    :return:
+    """
+
+
+def create_unique_index(cr, indexname, tablename, expressions):
+    """
+
+    :param cr:
+    :param indexname:
+    :param tablename:
+    :param expressions:
+    :return:
+    """
+
+
+def drop_index(cr, indexname, tablename):
+    """
+
+    :param cr:
+    :param indexname:
+    :param tablename:
+    :return:
+    """
+
+
+def drop_view_if_exists(cr, viewname):
+    """
+
+    :param cr:
+    :param viewname:
+    :return:
+    """
